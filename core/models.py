74e6aa6a9e9aa8976cc63324d195b7701079408a
root <root@DESKTOP-SRLN7RJ>
2023-03-29 16:08:46 +0100

    course model added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+
+class Result(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    marks = models.DecimalField(max_digits=5, decimal_places=2)
